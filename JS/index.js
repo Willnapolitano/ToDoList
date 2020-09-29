ad35ca7ea2a3fae3919b7d9031819fe79c4f080d
Will <wstechjunior@gmail.com>
2020-09-29 09:10:50 -0300

    Adding items to the list

--- JS/index.js
+++ JS/index.js
@@ -1,16 +1,20 @@
 let button = document.querySelector('#button1')
+let cont = 0
 
 button.addEventListener('click', () => {
     let txt1 = document.querySelector('#text1').value
     adicionar_lista(txt1)
-    console.log(txt1)
 })
 
 function adicionar_lista(toDo){
+    cont++
     let txt2 = document.querySelector('#txt2')
     txt2.innerHTML += `
-        <p id='item'>${toDo}</p>
-    `
+        <div id="itens">
+            <input type="checkbox" name="item${cont}" id="item${cont}">
+            <label for="item${cont}">${toDo}</label>
+        </div>
+            `
 }
 
 
