6c74677b94794a6ffbce7d284831cbcdc7846c34
Will <wstechjunior@gmail.com>
2020-10-27 13:47:46 -0300

    Add same item filter

--- JS/index.js
+++ JS/index.js
@@ -3,25 +3,21 @@ let txt1 = []
 let cont = 0
 
 button.addEventListener('click', () => {
-    if(!filtro(txt1) && cont <= 10){
-        adicionar_lista(txt1)
+    if(cont <= 10){
+        adicionar_array(txt1)
     }else{
-        if(filtro(txt1)){
-            alert('Você ja adicionou esse item')
-        }else{
-            alert(
+        alert(
                 "Faça as tarefas que você já adicionou!\nLembre-se nenhum ser humano é multitarefa ;D"
             )
         }
     }  
-})
+)
 
-function adicionar_lista(toDo){
+function adicionar_lista(){
     let txt2 = document.querySelector('#txt2')
     let div = document.createElement("div")
     let input = document.createElement('input')
     let label = document.createElement('label')
-    txt1.push(document.querySelector('#text1').value)
     div.setAttribute('id','itens')
     input.setAttribute('type', 'checkbox')
     input.setAttribute('name',`ìtem${cont}`)
@@ -34,9 +30,30 @@ function adicionar_lista(toDo){
     cont++
 }
 
-function filtro(txt1){
-       return (new Set(txt1)).size != txt1.length
+function adicionar_array(txt1){
+    txt1.push(document.querySelector('#text1').value)
+    let Isrepetido = false
+    for(var j = 0; j < txt1.length; j++){
+        for(var l = j+1; l < txt1.length; l++){
+            if(txt1[j] == txt1[l]){
+                txt1.pop()
+                aviso_item_repetido()
+                Isrepetido = true
+            }else{
+                Isrepetido = false
+            }
+        }
+    }
+    if(!Isrepetido){
+        adicionar_lista(txt1)
+    }
+    
 }
+
+function aviso_item_repetido(){
+    alert("Você já adicionou este item.")
+}
+
 
 
 
