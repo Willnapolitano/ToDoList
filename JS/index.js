dd06d15a64482d82de549f5fb215a8cc7dd65921
Will <wstechjunior@gmail.com>
2020-10-28 09:09:47 -0300

    Add 'limpar_input' function

--- JS/index.js
+++ JS/index.js
@@ -5,6 +5,7 @@ let cont = 0
 button.addEventListener('click', () => {
     if(cont <= 10){
         adicionar_array(txt1)
+        limpar_input()
     }else{
         alert(
                 "Faça as tarefas que você já adicionou!\nLembre-se nenhum ser humano é multitarefa ;D"
@@ -18,18 +19,32 @@ function adicionar_lista(){
     let div = document.createElement("div")
     let input = document.createElement('input')
     let label = document.createElement('label')
+    let img = document.createElement('img')
+    img.setAttribute('src','/58326.png')
+    img.setAttribute('id',`img${cont}`)
     div.setAttribute('id','itens')
     input.setAttribute('type', 'checkbox')
     input.setAttribute('name',`ìtem${cont}`)
     input.setAttribute('id',`item${cont}`)
     label.setAttribute('for',`item${cont}`)
     label.innerHTML += `${txt1[cont]}`
+    img.style.display = 'none'
     txt2.appendChild(div)
     div.appendChild(input)
     div.appendChild(label)
+    div.appendChild(img)
     cont++
+    div.addEventListener('mouseover', () => {
+        img.style.display = 'block'
+    })
+    div.addEventListener('mouseout', () => {
+        img.style.display = 'none'
+    })
 }
 
+function limpar_input(){
+    document.getElementById('text1').value = ''
+}
 function adicionar_array(txt1){
     txt1.push(document.querySelector('#text1').value)
     let Isrepetido = false
